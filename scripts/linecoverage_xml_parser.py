199b7d9c70a6792eb3b506c6e7ec346c78ac9004
Akhil Giridhar <akhilgiridhar007@gmail.com>
2023-11-09 11:38:53 -0600

    Modified parsing scripts to output information to csv and reduce verbosity

--- scripts/linecoverage_xml_parser.py
+++ scripts/linecoverage_xml_parser.py
@@ -1,25 +1,22 @@
 import xml.etree.ElementTree as ET
 import os
+import csv
+from itertools import combinations
+
 
 def extract_blocks_from_xml(xml_file_path):
     """Extract blocks from the XML file."""
     tree = ET.parse(xml_file_path)
     root = tree.getroot()
-
     block_data = {}
-
     for block in root.findall('block'):
         block_classname = block.attrib['classname']
         block_method = block.attrib['method']
         block_number = block.attrib['number']
-
-        # Extracting tests
         tests = [test.attrib['name'] for test in block.find('tests').findall('test')]
-
-        # Using a tuple to store block data and associate it with its tests
         block_data[(block_classname, block_method, block_number)] = tests
-
     return block_data
+
 
 def compare_xml_reports(xml_file_1, xml_file_2):
     """Compare two XML reports for block tests coverage."""
@@ -27,36 +24,37 @@ def compare_xml_reports(xml_file_1, xml_file_2):
     block_data_2 = extract_blocks_from_xml(xml_file_2)
 
     differences = []
-
     for block_key in block_data_1:
         if block_key not in block_data_2 or set(block_data_1[block_key]) != set(block_data_2[block_key]):
             differences.append((block_key, block_data_1[block_key], block_data_2.get(block_key, None)))
-
     return differences
 
-if __name__ == '__main__':
-    directory = "TestReports"
-    xml_files = [os.path.join(directory, file) for file in os.listdir(directory) if file.endswith(".xml") and file.startswith("linecoverage")]
 
-    # Ensure that only 10 reports are considered (the first 10 found if more are present)
-    xml_files = xml_files[:10]
-
-    for i in range(len(xml_files)):
-        for j in range(i+1, len(xml_files)):
-            print(f"Comparing {xml_files[i]} with {xml_files[j]}")
-            
-            differences = compare_xml_reports(xml_files[i], xml_files[j])
+def write_differences_to_csv(output_file, xml_files):
+    with open(output_file, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(["XML File 1", "XML File 2", "Classname", "Method", "Number", "Tests in File 1", "Tests in File 2"])
+        for xml_file_1, xml_file_2 in xml_files:
+            differences = compare_xml_reports(xml_file_1, xml_file_2)
             if differences:
                 for diff in differences:
                     block_info, tests_1, tests_2 = diff
                     block_classname, block_method, block_number = block_info
+                    writer.writerow([os.path.basename(xml_file_1), os.path.basename(xml_file_2), block_classname, block_method, block_number, ', '.join(tests_1), ', '.join(tests_2) if tests_2 else 'None'])
 
-                    print(f"Difference found: Classname - {block_classname}, Method - {block_method}, Number - {block_number}")
-                    print(f"Tests in {xml_files[i]}: {', '.join(tests_1)}")
-                    if tests_2:
-                        print(f"Tests in {xml_files[j]}: {', '.join(tests_2)}")
-                    else:
-                        print(f"Tests in {xml_files[j]}: None")
-                    print("----------")
-            print("===================================")
+
+if __name__ == '__main__':
+    directory = "TestReports"
+    xml_files = [os.path.join(directory, file) for file in os.listdir(directory) if file.endswith(".xml") and file.startswith("linecoverage")]
+    xml_files = xml_files[:10]
+    pairs = list(combinations(xml_files, 2))  
+    output_file = "linecoverage_differences.csv"
+    write_differences_to_csv(output_file, pairs)
+    print(f"Differences written to {output_file}")
+
+
+
+
+
+
 
--- scripts/mutation_xml_parser.py
+++ scripts/mutation_xml_parser.py
@@ -1,11 +1,11 @@
 import xml.etree.ElementTree as ET
 import os
+import csv
 
 def extract_mutations_from_xml(xml_file_path):
     """Extract mutations from the XML file."""
     tree = ET.parse(xml_file_path)
     root = tree.getroot()
-
     mutations_data = {}
 
     for mutation in root.findall('mutation'):
@@ -16,14 +16,17 @@ def extract_mutations_from_xml(xml_file_path):
         mutator = mutation.find('mutator').text
         description = mutation.find('description').text
 
-        # Extracting killing and succeeding tests
-        killing_tests = mutation.find('killingTests').text.split('|') if mutation.find('killingTests').text else []
-        succeeding_tests = mutation.find('succeedingTests').text.split('|') if mutation.find('succeedingTests').text else []
+        killing_tests_element = mutation.find('killingTests')
+        killing_tests = killing_tests_element.text.split('|') if killing_tests_element is not None and killing_tests_element.text is not None else []
 
-        # Using a tuple to store mutation data and associate it with its tests
+        succeeding_tests_element = mutation.find('succeedingTests')
+        succeeding_tests = succeeding_tests_element.text.split('|') if succeeding_tests_element is not None and succeeding_tests_element.text is not None else []
+
         mutations_data[(status, mutated_class, mutated_method, line_number, mutator, description)] = (killing_tests, succeeding_tests)
 
     return mutations_data
+
+
 
 
 def compare_xml_reports(xml_file_1, xml_file_2):
@@ -32,72 +35,70 @@ def compare_xml_reports(xml_file_1, xml_file_2):
     mutations_data_2 = extract_mutations_from_xml(xml_file_2)
 
     differences = []
-
-    # Find mutations that have different tests between the two reports
     for mutation_key in mutations_data_1:
         if mutation_key in mutations_data_2:
             killing_tests_1, succeeding_tests_1 = mutations_data_1[mutation_key]
             killing_tests_2, succeeding_tests_2 = mutations_data_2[mutation_key]
-
-            # Convert lists to sets for comparison
             set_killing_1 = set(killing_tests_1)
             set_killing_2 = set(killing_tests_2)
-
             set_succeeding_1 = set(succeeding_tests_1)
             set_succeeding_2 = set(succeeding_tests_2)
-
-            # Check for differences between the sets
             if set_killing_1 != set_killing_2 or set_succeeding_1 != set_succeeding_2:
-                differences.append((mutation_key, (killing_tests_1, succeeding_tests_1), (killing_tests_2, succeeding_tests_2)))
+                differences.append((mutation_key, set_killing_1 - set_killing_2, set_killing_2 - set_killing_1, set_succeeding_1 - set_succeeding_2, set_succeeding_2 - set_succeeding_1))
 
     return differences
+
+
+
+
+def write_differences_to_csv(output_file, xml_files):
+    with open(output_file, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(["XML File 1", "XML File 2", "Status", "Mutated Class", "Mutated Method", "Line Number", "Mutator", "Description", "Unique Killing Tests 1", "Unique Killing Tests 2", "Unique Succeeding Tests 1", "Unique Succeeding Tests 2"])
+
+
+        for xml_file_1, xml_file_2 in xml_files:
+            differences = compare_xml_reports(xml_file_1, xml_file_2)
+            if differences:
+                for mutation_info, unique_killing_1, unique_killing_2, unique_succeeding_1, unique_succeeding_2 in differences:
+                    status, mutated_class, mutated_method, line_number, mutator, description = mutation_info
+                    writer.writerow([os.path.basename(xml_file_1), os.path.basename(xml_file_2), status, mutated_class, mutated_method, line_number, mutator, description, ', '.join(unique_killing_1), ', '.join(unique_killing_2), ', '.join(unique_succeeding_1), ', '.join(unique_succeeding_2)])
+
+
 
 
 
 if __name__ == '__main__':
     directory = "TestReports"
     xml_files = [os.path.join(directory, file) for file in os.listdir(directory) if file.endswith(".xml") and file.startswith("mutations")]
+    pairs = [(xml_files[i], xml_files[j]) for i in range(len(xml_files)) for j in range(i+1, len(xml_files))]
+    differences = []
+    for xml_file_1, xml_file_2 in pairs:
+        temp_differences = compare_xml_reports(xml_file_1, xml_file_2)
+        if temp_differences:
+            differences.extend(temp_differences)
+    output_file = "mutation_differences.csv"
 
 
-    for i in range(len(xml_files)):
-        for j in range(i+1, len(xml_files)):
-            print(f"Comparing {xml_files[i]} with {xml_files[j]}")
+    with open(output_file, 'w', newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(["XML File 1", "XML File 2", "Status", "Mutated Class", "Mutated Method", "Line Number", "Mutator", "Description", "Unique Killing Tests 1", "Unique Killing Tests 2", "Unique Succeeding Tests 1", "Unique Succeeding Tests 2"])
 
-            differences = compare_xml_reports(xml_files[i], xml_files[j])
 
-            if differences:
-                for diff in differences:
-                    mutation_info, tests_1, tests_2 = diff
-                    status, mutated_class, mutated_method, line_number, mutator, description = mutation_info
+        for diff in differences:
+            mutation_info, unique_killing_1, unique_killing_2, unique_succeeding_1, unique_succeeding_2 = diff
+            status, mutated_class, mutated_method, line_number, mutator, description = mutation_info
+            writer.writerow([os.path.basename(xml_file_1), os.path.basename(xml_file_2), status, mutated_class, mutated_method, line_number, mutator, description, ', '.join(unique_killing_1), ', '.join(unique_killing_2), ', '.join(unique_succeeding_1), ', '.join(unique_succeeding_2)])
+           
+    print(f"Differences written to {output_file}")
 
-                    killing_tests_1, succeeding_tests_1 = tests_1
-                    killing_tests_2, succeeding_tests_2 = tests_2
 
-                    # Convert lists to sets for comparison
-                    set_killing_1 = set(killing_tests_1)
-                    set_killing_2 = set(killing_tests_2)
 
-                    set_succeeding_1 = set(succeeding_tests_1)
-                    set_succeeding_2 = set(succeeding_tests_2)
 
-                    # Determine unique tests in each XML file
-                    unique_killing_1 = set_killing_1 - set_killing_2
-                    unique_killing_2 = set_killing_2 - set_killing_1
 
-                    unique_succeeding_1 = set_succeeding_1 - set_succeeding_2
-                    unique_succeeding_2 = set_succeeding_2 - set_succeeding_1
 
-                    print(f"Difference found: Class - {mutated_class}, Method - {mutated_method}, Line - {line_number}, Mutator - {mutator}, Status - {status}")
 
-                    if unique_killing_1:
-                        print(f"Unique killing tests in {xml_files[i]}: {', '.join(unique_killing_1)}")
-                    if unique_killing_2:
-                        print(f"Unique killing tests in {xml_files[j]}: {', '.join(unique_killing_2)}")
 
-                    if unique_succeeding_1:
-                        print(f"Unique succeeding tests in {xml_files[i]}: {', '.join(unique_succeeding_1)}")
-                    if unique_succeeding_2:
-                        print(f"Unique succeeding tests in {xml_files[j]}: {', '.join(unique_succeeding_2)}")
 
-                    print("----------")
-            print("===================================")
+
+
